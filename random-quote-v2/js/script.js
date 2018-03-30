a50e2aa0c847cf06eb2414365633ae82c145b237
shaun deshler <shaundeshler@shauns-MacBook-Pro.local>
2018-03-30 14:15:07 -0700

    set event listener and timer for the functions

--- random-quote-v2/js/script.js
+++ random-quote-v2/js/script.js
@@ -1,6 +1,7 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 
+// stored quotes and sources in a object and placed the objects in a array
 const quotes = [
   {
     quote: "Do not let your hearts be troubled. Trust in God; trust also in me.",
@@ -24,13 +25,25 @@ const quotes = [
   }
 ];
 
+// this function will take our array of objects and create a random number then apply that random number to our array index
 let getRandomQuote = (arr) => {
   let randomNum = Math.floor(Math.random() * arr.length);
   return arr[randomNum];
 }
 
+// this function will take our getRandomQuote function with our random array object then build a string with our object data and insert it to the DOM
   let printQuote = (fn) => {
   let callBackQuote = fn(quotes);
   let str = `<p class="quote"> ${callBackQuote.quote} </p> <p class="source"> ${callBackQuote.source} </p>`
   document.getElementById('quote-box').innerHTML = str;
 }
+
+// this will call our constucted function every 10 seconds
+setInterval(function(){printQuote(getRandomQuote)}, 10000);
+
+/* side note this only works if i place the printQuote function in another function
+inside the setInterval method i believe this is a closure problem but will come back to it later
+*/
+
+// this will add a click event listener to the button with the loadQuote id and will call the constucted printQuote function every time the button is clicked
+document.getElementById('loadQuote').addEventListener("click", function(){printQuote(getRandomQuote)}, false);
